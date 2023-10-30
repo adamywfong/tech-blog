31968d4757963c379c264828810254810cc0ce06
afong <adam.y.w.fong@gmail.com>
2023-10-29 20:23:54 -0700

    created Post and Comment models

--- models/index.js
+++ models/index.js
@@ -1,9 +1,32 @@
-const router = require('express').Router();
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
 
-module.exports = router;
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL',
+});
+
+module.exports = {User, Post, Comment,};
